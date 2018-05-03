491b3113354185a8d5d6c2d9233b2de9b66fd123
JackZhu <435999799@qq.com>
2018-05-03 19:04:05 +0800

    cloud配置切换策略

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/ulovecode/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ulovecode.springcloud.service.impl;
+
+import java.util.List;
+
+import com.ulovecode.springcloud.dao.DeptDao;
+import com.ulovecode.springcloud.entities.Dept;
+import com.ulovecode.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @author JackZhu
+ * @since 2018/05/02
+ */
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
